c34b16ea9c800fc24cc6c0d84a53e82199a81610
bogdan pryvalov <bohdan.privalov@tech-stack.io>
2021-07-17 22:02:04 +0300

    added handlers on input

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-function inputWithCross({ id, color, parentId }) {
-	let input = document.querySelector(`#${id}`);
-	const parentElem = document.querySelector(`#${parentId}`);
+function inputWithCross({ id, color, parentId, onClick, onFocus, onblur }) {
+	if (!parentId) {
+		return new Error('You need to set parent id');
+	}
 
+	const parentElem = document.querySelector(`#${parentId}`);
+	let input = document.createElement('input');
+
+	input.setAttribute('id', id);
+	input.onclick = onClick;
+	input.onfocus = onFocus;
+	input.onblur = onblur;
 	input.style.background = color;
 
 	parentElem.appendChild(input);
